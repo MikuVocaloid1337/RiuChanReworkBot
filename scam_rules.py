fa63840f6f7050c73720a97d3f91c4340170ae45
MikuVocaloid1337 <96827003+MikuVocaloid1337@users.noreply.github.com>
2025-05-06 12:33:50 +0400

    Create scam_rules.py

---
+++ scam_rules.py
@@ -0,0 +1,18 @@
+# scam_rules.py
+
+SCAM_KEYWORDS = [
+    "заработок", "деньги", "инвестиции", "быстрый доход",
+    "пассивный доход", "без вложений", "прямо сейчас", "гарантированно",
+    "доход от", "удаленная работа", "ставки", "1xbet", "казино", "бинанс", "crypto", "бинарные опционы"
+]
+
+SCAM_DOMAINS = [
+    "t.me/", "bit.ly/", "binance", "1xbet", "rich-", "profit", "earn", "drop", "crypto", "invest", "fastmoney"
+]
+
+import re
+SCAM_PATTERNS = [
+    re.compile(r"зараб.*\d+\s*(руб|₽|доллар|$)", re.IGNORECASE),
+    re.compile(r"(инвест|вложи).*гарант", re.IGNORECASE),
+    re.compile(r"(https?:\/\/)?(www\.)?(t\.me|bit\.ly|tinyurl|taplink)\.com\/[^\s]+", re.IGNORECASE),
+]
